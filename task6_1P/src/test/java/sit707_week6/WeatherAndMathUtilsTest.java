0fe4e448ce8d41cae55cf53dfd426473a5ab085c
andre <andre@DESKTOP-JO1G81O>
2025-04-23 00:25:06 +1000

    Import Task 6.1P

---
+++ task6_1P/src/test/java/sit707_week6/WeatherAndMathUtilsTest.java
@@ -0,0 +1,32 @@
+package sit707_week6;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WeatherAndMathUtilsTest {
+	
+	@Test
+	public void testStudentIdentity() {
+		String studentId = "s222225013";
+		Assert.assertNotNull("Student ID is null", studentId);
+	}
+
+	@Test
+	public void testStudentName() {
+		String studentName = "Andrei";
+		Assert.assertNotNull("Student name is null", studentName);
+	}
+	
+	@Test
+	public void testFalseNumberIsEven() {
+		Assert.fail("Not yet implemented");
+	}
+	
+    @Test
+    public void testCancelWeatherAdvice() {
+    	Assert.assertEquals("CANCEL", WeatherAndMathUtils.weatherAdvice(70.1, 0.0));
+    }
+    
+    
+    
+}
