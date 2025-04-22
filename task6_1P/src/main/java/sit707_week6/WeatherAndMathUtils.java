8ce1728318db626161059b442e8978e3dfd037d5
andre <andre@DESKTOP-JO1G81O>
2025-04-23 00:57:22 +1000

    Fixing IsPrime() and Asserting more Test Cases for Coverage Tasks

--- task6_1P/src/main/java/sit707_week6/WeatherAndMathUtils.java
+++ task6_1P/src/main/java/sit707_week6/WeatherAndMathUtils.java
@@ -51,17 +51,16 @@ public class WeatherAndMathUtils {
 	 * @param n
 	 * @return
 	 */
-	public static boolean isPrime(int n) {
-		if (n == 1) {
-			return true;
+	public static boolean isPrime(int n) 
+	{
+		if (n <= 1) return false;
+		if (n == 2) return true;
+		if (n % 2 == 0) return false;
+		for (int i = 3; i <= Math.sqrt(n); i += 2) 
+		{
+			if (n % i == 0) return false;
 		}
-		// Test if n is divisible by any other number down to 2.
-		for (int i = 2; i < n; i++) {
-			if (isEven(n)) {
-				return false;
-			}
-		}
-		// Number is prime
 		return true;
 	}
+
 }
--- task6_1P/src/test/java/sit707_week6/WeatherAndMathUtilsTest.java
+++ task6_1P/src/test/java/sit707_week6/WeatherAndMathUtilsTest.java
@@ -18,15 +18,85 @@ public class WeatherAndMathUtilsTest {
 	}
 	
 	@Test
-	public void testFalseNumberIsEven() {
-		Assert.fail("Not yet implemented");
+	public void testFalseNumIsEvenNum() {
+		Assert.assertFalse(WeatherAndMathUtils.isEven(7));
+
 	}
 	
     @Test
-    public void testCancelWeatherAdvice() {
+    public void testCancelWeatAdvice() {
     	Assert.assertEquals("CANCEL", WeatherAndMathUtils.weatherAdvice(70.1, 0.0));
     }
     
-    
+    @Test
+    public void testWeatAdviceCancel_Rain() {
+        Assert.assertEquals("CANCEL", WeatherAndMathUtils.weatherAdvice(10.0, 6.1));
+    }
+
+    @Test
+    public void testWeatAdviceCancel_Both() {
+        Assert.assertEquals("CANCEL", WeatherAndMathUtils.weatherAdvice(46.0, 4.1));
+    }
+
+    @Test
+    public void testWeatAdviceWarn_Wind() {
+        Assert.assertEquals("WARN", WeatherAndMathUtils.weatherAdvice(46.0, 1.0));
+    }
+
+    @Test
+    public void testWeatAdviceWarn_Rain() {
+        Assert.assertEquals("WARN", WeatherAndMathUtils.weatherAdvice(10.0, 5.0));
+    }
+
+    @Test
+    public void testWeatAdviceAllClear() {
+        Assert.assertEquals("ALL CLEAR", WeatherAndMathUtils.weatherAdvice(10.0, 1.0));
+    }
+
+    @Test
+    public void testWeatAdviceNegativeInputs() {
+        try {
+            WeatherAndMathUtils.weatherAdvice(-1.0, 0.0);
+            Assert.fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            // Passed
+        }
+    }
+
+    @Test
+    public void testIsEven_TrueNum() {
+        Assert.assertTrue(WeatherAndMathUtils.isEven(400));
+    }
+
+    @Test
+    public void testIsEven_FalseNum() {
+        Assert.assertFalse(WeatherAndMathUtils.isEven(101));
+    }
+
+    @Test
+    public void testIsEven_ZeroNum() {
+        Assert.assertTrue(WeatherAndMathUtils.isEven(0));
+    }
+
+    @Test
+    public void testIsPrime_TrueNum() {
+        Assert.assertTrue(WeatherAndMathUtils.isPrime(43));
+    }
+
+    @Test
+    public void testIsPrime_FalseNum() {
+        Assert.assertFalse(WeatherAndMathUtils.isPrime(-7));
+    }
+
+    @Test
+    public void testIsPrime_ZeroNum() {
+        Assert.assertFalse(WeatherAndMathUtils.isPrime(0));
+    }
+
+    @Test
+    public void testIsPrime_NegativeNum() {
+        Assert.assertFalse(WeatherAndMathUtils.isPrime(-97));
+    }
+
     
 }
